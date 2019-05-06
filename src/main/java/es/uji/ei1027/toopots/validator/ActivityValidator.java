f7f27a499ebdb0c79374db85203987b1cc1d2ab4
Ferran Ramia <al361866@uji.es>
2019-05-06 13:58:50 +0200

    ActivityValidator

--- src/main/java/es/uji/ei1027/toopots/validator/ActivityValidator.java
+++ src/main/java/es/uji/ei1027/toopots/validator/ActivityValidator.java
@@ -17,5 +17,42 @@ public class ActivityValidator implements Validator {
     public void validate(Object obj, Errors errors) {
         Activity activity = (Activity) obj;
 
+        if (activity.getName().trim().equals(""))
+            errors.rejectValue("name", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getDates() == null)
+            errors.rejectValue("dates", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getPlace().trim().equals(""))
+            errors.rejectValue("place", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getMinNumberPeople() == 0)
+            errors.rejectValue("minNumberPeople", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getMaxNumberPeople() == 0)
+            errors.rejectValue("maxNumberPeople", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getPricePerPerson() == 0)
+            errors.rejectValue("pricePerPerson", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getMeetingPoint().trim().equals(""))
+            errors.rejectValue("meetingPoint", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getMeetingTime() == null)
+            errors.rejectValue("meetingTime", "obligatori",
+                    "Cal introduir un valor");
+
+        if (activity.getDescription().trim().equals(""))
+            errors.rejectValue("description", "obligatori",
+                    "Cal introduir un valor");
+
+
     }
 }
