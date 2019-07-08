0d22874a57fe0101b7caafbae1629b7a0969d453
al361880 <37330374+al361880@users.noreply.github.com>
2019-07-08 16:55:08 +0200

    F

--- src/main/java/es/uji/ei1027/toopots/TooPotsConfiguration.java
+++ src/main/java/es/uji/ei1027/toopots/TooPotsConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.Formatter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.sql.DataSource;
 import java.text.ParseException;
@@ -14,7 +16,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 @Configuration
-public class TooPotsConfiguration {
+public class TooPotsConfiguration implements WebMvcConfigurer {
 
     // Configura l'accés a la base de dades (DataSource)
     // a partir de les propietats a src/main/resources/applications.properties
@@ -53,5 +55,11 @@ public class TooPotsConfiguration {
                 return DateTimeFormatter.ISO_LOCAL_TIME.format(object);
             }
         };
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/certificates/**").addResourceLocations("file:///C:/Users/usuario/Desktop/Escritorio/3º informatica/uji/EI1027_Disseny_impl_SI/Projecte/TooPots/src/main/resources/static/certificates").setCachePeriod(0);
+        registry.addResourceHandler("/images/**").addResourceLocations("file:///C:/Users/usuario/Desktop/Escritorio/3º informatica/uji/EI1027_Disseny_impl_SI/Projecte/TooPots/src/main/resources/static/images").setCachePeriod(0);
     }
 }
